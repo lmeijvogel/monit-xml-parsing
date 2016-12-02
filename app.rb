601127a49fed5fe9a10325c5b599949969d3c2f0
Lennaert Meijvogel <l.meijvogel@websend.nl>
2016-12-02 11:19:34 +0100

    Display build status along with server status.

--- app.rb
+++ app.rb
@@ -2,8 +2,12 @@ require 'sinatra'
 require 'nokogiri'
 require 'sinatra/reloader'
 
+require 'dotenv'
+
 require 'erb'
 require 'net/http'
+
+Dotenv.load
 
 set :bind, '0.0.0.0'
 set :port, 9292
@@ -22,16 +26,24 @@ unless is_http?(source)
 end
 
 get '/' do
-  @statuses = read_xml(source)
+  @build_display_url = ENV.fetch("BUILD_DISPLAY_URL")
+  @monit_status_container_url = ENV.fetch("MONIT_STATUS_CONTAINER_URL")
 
   renderer = ERB.new(root_template)
   renderer.result(binding)
 end
 
-get '/status' do
+get '/monit' do
+  @monit_status_url = ENV.fetch("MONIT_STATUS_URL")
+
+  renderer = ERB.new(monit_template)
+  renderer.result(binding)
+end
+
+get '/monit_status' do
   @statuses = read_xml(source)
 
-  renderer = ERB.new(status_template)
+  renderer = ERB.new(monit_status_template)
   renderer.result(binding)
 end
 
@@ -64,123 +76,158 @@ def get_from_http(source)
 
   res.body
 end
+
 def root_template
   <<-TEMPLATE
     <html>
       <head>
-      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"></link>
-      <style>
-        body {
-          background-color: black;
-        }
-
-        .server-list {
-          margin-top: 5px;
-        }
-
-        .server-line {
-          margin-top: 1px;
-          margin-bottom: 0px;
-        }
-
-        .server-button {
-          width: 300px;
-          font-size: 20pt;
-        }
-
-        .server-button.blink-on {
-          color: #d9534f;
-        }
-      </style>
-
-      <script>
-        function doRequest() {
-          getJson('/status', {
-            success: function (data) {
-              document.querySelector('.server-list').innerHTML = data;
-            },
-
-            error: function (data) {
-              var serverLines = document.querySelectorAll('.server-button');
-
-              if (serverLines) {
-                serverLines.forEach( function (line) {
-                  line.innerHTML = "?";
-                  line.className = "server-button btn btn-warning";
-                });
-              } else {
-                document.querySelector('.server-list').innerHTML = '<button class="btn btn-warning">ERROR</button>';
-              }
-            }
-          });
-        }
-
-        function getJson(uri, options) {
-          var request = new window.XMLHttpRequest();
-
-          request.open('GET', uri, true);
-
-          request.onload = function () {
-            if (request.status >= 200 && request.status < 400) {
-              if (options.success) {
-                options.success(request.responseText);
-              }
-            } else {
-              if (options.error) {
-                options.error(data);
-              }
-            }
-          };
-
-          request.onerror = function () {
-            if (options.error) {
-              options.error();
-            }
-          };
-
-          request.send();
-        }
-
-        function animateUnavailableServers() {
-          var blinkOn = false;
-
-          setInterval(function () {
-            if (blinkOn) {
-              var buttons = document.querySelectorAll(".btn");
-
-              buttons.forEach(function (button) {
-                button.className = button.className.replace("blink-on", "");
-              });
-            } else {
-              var unavailableButtons = document.querySelectorAll(".btn-danger");
-
-              unavailableButtons.forEach(function (button) {
-                button.className = button.className + " blink-on";
-              });
-            }
-
-            blinkOn = !blinkOn;
-          }, 1000);
-        }
-      </script>
-    </head>
-    <body>
-      <div class="server-list"></div>
-    </body>
-    <script>
-      doRequest();
-
-      setInterval(function () {
-        doRequest();
-      }, 30000);
-
-      animateUnavailableServers();
-    </script>
-  </html>
+        <style>
+          html, body {
+            height: 100%;
+            background-color: black;
+            overflow-y: hidden;
+            overflow-x: hidden;
+          }
+          iframe {
+            height: 100%;
+            margin: 0;
+            padding: 0;
+            border: 0;
+          }
+          .builds {
+            width: calc(100% - 320px);
+          }
+          .servers {
+            width: 300px;
+          }
+        </style>
+      </head>
+      <body>
+        <iframe class="builds" src="<%= @build_display_url %>"></iframe>
+        <iframe class="servers" src="<%= @monit_status_container_url %>"></iframe>
+        <!-- iframe class="servers" src="http://10.0.4.91:9292"></iframe -->
+      </body>
+    </html>
   TEMPLATE
 end
 
-def status_template
+def monit_template
+  <<-TEMPLATE
+    <html>
+      <head>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"></link>
+        <style>
+          body {
+            background-color: black;
+          }
+
+          .server-list {
+            margin-top: 5px;
+          }
+
+          .server-line {
+            margin-top: 1px;
+            margin-bottom: 0px;
+          }
+
+          .server-button {
+            width: 300px;
+            font-size: 20pt;
+          }
+
+          .server-button.blink-on {
+            color: #d9534f;
+          }
+        </style>
+
+        <script>
+          function doRequest() {
+            getJson('<%= @monit_status_url %>', {
+              success: function (data) {
+                document.querySelector('.server-list').innerHTML = data;
+              },
+
+              error: function (data) {
+                var serverLines = document.querySelectorAll('.server-button');
+
+                if (serverLines) {
+                  serverLines.forEach( function (line) {
+                    line.innerHTML = "?";
+                    line.className = "server-button btn btn-warning";
+                  });
+                } else {
+                  document.querySelector('.server-list').innerHTML = '<button class="btn btn-warning">ERROR</button>';
+                }
+              }
+            });
+          }
+
+          function getJson(uri, options) {
+            var request = new window.XMLHttpRequest();
+
+            request.open('GET', uri, true);
+
+            request.onload = function () {
+              if (request.status >= 200 && request.status < 400) {
+                if (options.success) {
+                  options.success(request.responseText);
+                }
+              } else {
+                if (options.error) {
+                  options.error(data);
+                }
+              }
+            };
+
+            request.onerror = function () {
+              if (options.error) {
+                options.error();
+              }
+            };
+
+            request.send();
+          }
+
+          function animateUnavailableServers() {
+            var blinkOn = false;
+
+            setInterval(function () {
+              if (blinkOn) {
+                var buttons = document.querySelectorAll(".btn");
+
+                buttons.forEach(function (button) {
+                  button.className = button.className.replace("blink-on", "");
+                });
+              } else {
+                var unavailableButtons = document.querySelectorAll(".btn-danger");
+
+                unavailableButtons.forEach(function (button) {
+                  button.className = button.className + " blink-on";
+                });
+              }
+
+              blinkOn = !blinkOn;
+            }, 1000);
+          }
+        </script>
+      </head>
+      <body>
+        <div class="server-list"></div>
+      </body>
+      <script>
+        doRequest();
+
+        setInterval(function () {
+          doRequest();
+        }, 30000);
+
+        animateUnavailableServers();
+      </script>
+    </html>
+  TEMPLATE
+end
+
+def monit_status_template
   <<-TEMPLATE
       <% @statuses.each do |status|
         button_class = status[:up] ? 'btn-success' : 'btn-danger' %>
